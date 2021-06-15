e7a8c8a110eb7df11268d39ed64c07801e4ab188
Philip Tran <tran.philip02@gmail.com>
2021-06-15 23:47:50 +0200

    Swipe

--- src/components/utils/Test.jsx
+++ src/components/utils/Test.jsx
@@ -1,34 +1,58 @@
 import { React, useEffect, useState } from 'react';
-import { Button, Container, Divider, Paper, makeStyles } from '@material-ui/core';
+import { Button, Container, Divider, Paper, makeStyles, Typography } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
-
+    outerContainer: {
+        display: 'flex',
+        alignItems: 'center',
+    },
+    innerContainer: {
+        display: 'flex',
+        height: '30vh',
+        justifyContent: 'space-around'
+    },
+    filmContainer: {
+        width: '15%',
+        
+    },
+    currentFilm: {
+        width: '25%',
+    }
 }));
 
 const Test = () => {
     const classes = useStyles();
     const [activeCardIndex, setActiveCardIndex] = useState(2);
+    const [nextDisabled, setNextDisabled] = useState(false);
+    const [backDisabled, setBackDisabled] = useState(false);
     const [activeCards, setActiveCards] = useState([]);
     const [cards, setCards] = useState([
         {
+            index: 1,
             text: 'first',
         },
         {
+            index: 2,
             text: 'second',
         },
         {
+            index: 3,
             text: 'third',
         },
         {
+            index: 4,
             text: 'forth',
         },
         {
+            index: 5,
             text: 'fifth',
         },
         {
+            index: 6,
             text: 'sixth',
         },
         {
+            index: 7,
             text: 'seventh',
         },
     ]);
@@ -39,14 +63,19 @@ const Test = () => {
     }, [activeCardIndex]);
 
     const handleBack = () => {
-        if(activeCardIndex === 0) return;
+        if(activeCardIndex === 0) {return};
+        if(nextDisabled) setNextDisabled(false);
+        const newIndex = activeCardIndex - 1;
         setActiveCardIndex((prevIndex) => prevIndex - 1);
+        if(newIndex === 0) setBackDisabled(true);
     };
 
     const handleNext = () => {
-        console.log(cards.length)
-        if(activeCardIndex > cards.length - 1) return;
+        if(activeCardIndex >= cards.length - 1) {return};
+        if(backDisabled) setBackDisabled(false);
+        const newIndex = activeCardIndex + 1;
         setActiveCardIndex((prevIndex) => prevIndex + 1);
+        if(newIndex >= cards.length - 1) setNextDisabled(true);
     };
 
     const renderCards = (index) => {
@@ -55,23 +84,52 @@ const Test = () => {
         if(index < 2) {
             for (let i = 0; i <= index + 2; i++){
                 nums.push(i);
-            }    
+            };
         } else {
             for (let i = (index - 2); i <= index + 2; i++){
-                console.log(i)
                 nums.push(i);
-            }    
-        }
+            };  
+        };
         nums.forEach(num => {
+            if(cards[num] === undefined) return;
             cardsArray.push(cards[num]);
         });
-        console.log(cardsArray)
+        if(cardsArray.length < 5) {
+            if (cardsArray[0] === cards[0] || cardsArray[0].text === 'Nekecej'){
+                while (cardsArray.length < 5){
+                    const emptyObject = {text: 'Nekecej', index: Math.floor(Math.random() * 1000)};
+                    cardsArray.unshift(emptyObject);
+                    console.log(cardsArray);
+                };
+            } else {
+                while (cardsArray.length < 5){
+                    const emptyObject = {text: 'Nekecej', index: Math.floor(Math.random() * 1000)};
+                    cardsArray.push(emptyObject);
+                };    
+            }
+        };
+        setActiveCards([]);
+        setActiveCards(cardsArray);
+        cardsArray.forEach((card) => {
+            console.log(card);
+        });  
     };
 
+
+
     return (
-        <div>
-            <button onClick={handleBack}>Back</button>
-            <button onClick={handleNext}>Next</button>
+        <div className={classes.outerContainer}>
+            <Button onClick={handleBack} disabled={backDisabled}>Back</Button>
+            <Container maxWidth="lg" className={classes.innerContainer}>
+                {/* {activeCards.map((activeCard) => (
+                    <Paper className={classes.filmContainer} key={activeCard.index}><Typography>{activeCard.text}</Typography></Paper>
+                ))} */}
+                {activeCards.map((activeCard) => {
+                    if(activeCard === cards[activeCardIndex]) return (<Paper className={classes.currentFilm} key={activeCard.index}><Typography>{activeCard.text}</Typography></Paper>);
+                    return(<Paper className={classes.filmContainer} key={activeCard.index}><Typography>{activeCard.text}</Typography></Paper>);
+                })}
+            </Container>
+            <Button onClick={handleNext} disabled={nextDisabled}>Next</Button>
         </div>
     )
 }
