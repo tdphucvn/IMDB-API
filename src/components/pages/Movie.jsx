d8f9b0dcc7236d2de355f441dc9a536c93d2d67e
Philip Tran <tran.philip02@gmail.com>
2021-07-02 00:24:31 +0200

    Movie

--- src/components/pages/Movie.jsx
+++ src/components/pages/Movie.jsx
@@ -6,37 +6,92 @@ import MovieOverview from '../utils/movie/MovieOverview';
 
 const Movie = () => {
     const [movie, setMovie] = useState([]);
+    const [movieTrailer, setMovieTrailer] = useState([]);
+    const [movieCredit, setMovieCredit] = useState([]);
     const {id} = useParams();
     
-    const URL = `https://api.themoviedb.org/3/movie/${id}?api_key=ef7ddaa9270377970a055a19e5bfc2e5&language=en-US`;
+    const URL_DATA = `https://api.themoviedb.org/3/movie/${id}?api_key=ef7ddaa9270377970a055a19e5bfc2e5&language=en-US`;
+    const URL_VIDEOS = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=ef7ddaa9270377970a055a19e5bfc2e5&language=en-US`;
+    const URL_CREDIT = `https://api.themoviedb.org/3/movie/${id}/credits?api_key=ef7ddaa9270377970a055a19e5bfc2e5&language=en-US`;
+
+    const fetchData = async (URL, setter) => {
+        const method = {
+            method: 'GET', 
+            contentType: 'application/json',
+        };
+        await fetch(URL, method)
+        .then((res) => {
+            if (res.ok) return res.text();
+            return res.text().then(err => {
+                return Promise.reject({
+                    status: res.status,
+                    statusText: res.statusText,
+                    errorMessage: err,
+                });
+            });
+        })
+        .then((data) => {
+            setter(JSON.parse(data));
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    };
 
     useEffect(() => {
-        const fetchMovie = async () => {
-            await fetch(URL, {method: 'GET', contentType: 'application/json'})
-                .then((res) => {
-                    if (res.ok) return res.text();
-                    return res.text().then(err => {
-                        return Promise.reject({
-                            status: res.status,
-                            statusText: res.statusText,
-                            errorMessage: err,
-                        });
-                    });
-                })
-                .then((data) => {
-                    setMovie(JSON.parse(data));
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
-        }
-        fetchMovie();
-    }, [URL]);
+        fetchData(URL_DATA, setMovie);
+        fetchData(URL_VIDEOS, setMovieTrailer);
+        fetchData(URL_CREDIT, setMovieCredit);
+        // const fetchMovie = async () => {
+        //     await fetch(URL, {method: 'GET', contentType: 'application/json'})
+        //         .then((res) => {
+        //             if (res.ok) return res.text();
+        //             return res.text().then(err => {
+        //                 return Promise.reject({
+        //                     status: res.status,
+        //                     statusText: res.statusText,
+        //                     errorMessage: err,
+        //                 });
+        //             });
+        //         })
+        //         .then((data) => {
+        //             setMovie(JSON.parse(data));
+        //         })
+        //         .catch((err) => {
+        //             console.log(err);
+        //         });
+        // };
+
+        // const fetchMoviesVideos = async () => {
+        //     const method = {method: 'GET', contentType: 'application/json'};
+        //     await fetch(URL_VIDEOS, method)
+        //     .then((res) => {
+        //         if (res.ok) return res.text();
+        //         return res.text().then(err => {
+        //             return Promise.reject({
+        //                 status: res.status,
+        //                 statusText: res.statusText,
+        //                 errorMessage: err,
+        //             });
+        //         });
+        //     })
+        //     .then((data) => {
+        //         setMovieTrailer(JSON.parse(data));
+        //     })
+        //     .catch((err) => {
+        //         console.log(err);
+        //     });
+        // };
+        // fetchMovie();
+        // fetchMoviesVideos();
+    }, [id, URL_CREDIT, URL_DATA, URL_VIDEOS]);
+
+    console.log(movie, movieTrailer, movieCredit)
 
     return (
         <>
             <SingleHeader />
-            <MovieOverview movieData={movie}/>
+            <MovieOverview movieData={movie} trailer={movieTrailer.results}/>
         </>
     )
 }
--- src/components/utils/movie/MovieOverview.jsx
+++ src/components/utils/movie/MovieOverview.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Typography, makeStyles, Container } from '@material-ui/core';
+import { Typography, makeStyles, Container, CircularProgress, Box, Button } from '@material-ui/core';
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
+
 
 const useStyles = makeStyles((theme) => ({
     slider: {
@@ -8,24 +10,40 @@ const useStyles = makeStyles((theme) => ({
         left: 0,
         height: '100%',
         width: '100%',
-        background: 'linear-gradient(to right, #0d253f, transparent)',
-        opacity: 0.7
+        // background: 'linear-gradient(to right, #0d253f, transparent)',
+        background: '#0d253f',
+        opacity: 0.6
     },
     movieInfoContainer: {
         height: '100%',
-        '& *': {
-            height: 'inherit'
-        },
         display: 'flex',
+        alignItems: 'center',
         zIndex: '3',
         position: 'relative',
+    },
+    moviePoster: {
+        flex: 1,
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        '& img': {
+            height: '80%',
+            borderRadius: 10
+        }, 
+    },
+    movieInfo: {
+        flex: 2,
+        padding: 20,
+        color: 'white'
     }
 }));
 
-const MovieOverview = ({movieData}) => {
+const MovieOverview = ({movieData, trailer}) => {
+    const {key} = trailer[0];
     const classes = useStyles();
-
-    const {backdrop_path: backdrop, poster_path: poster, budget, genres, homepage, id: movieID, overview, vote_average: vote, release_date: release, title } = movieData;
+    const {backdrop_path: backdrop, poster_path: poster, budget, genres, homepage, id: movieID, overview, vote_average: vote, release_date: release, title, production_countries: countries, runtime } = movieData;
     const IMG_API_BACKDROP = 'https://image.tmdb.org/t/p/original/';
     const IMG_API_POSTER = 'https://image.tmdb.org/t/p/w500/';
 
@@ -39,8 +57,28 @@ const MovieOverview = ({movieData}) => {
                             <img src={IMG_API_POSTER + poster} alt="" />
                         </div>
                         <div className={classes.movieInfo}>
-                            <Typography variant="h4">{title} ({release.substring(0,4)})</Typography>
-                            <Typography variant="body1">{release}</Typography>
+                            <Typography variant="h4">{title} ({release && release.substring(0,4)})</Typography>
+                            <Typography variant="body1" gutterBottom={true}>{release} / <span>({countries && countries.map((country, countryIndex) => (<span key={countryIndex}>{country.name}</span>))})</span> / <span>{genres && genres.map((genre, index) => (<span>{genre.name}{genres.length-1 > index ? ', ' : ''}</span>))}</span> / <span>{runtime && runtime + 'min'}</span></Typography>
+                            <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                                <div style={{position: 'relative', display: 'inline-flex'}}>
+                                    <CircularProgress variant="determinate" value={vote * 10} size="3.8rem" color="secondary" thickness={5}/>
+                                    <Box
+                                        top={0}
+                                        left={0}
+                                        bottom={0}
+                                        right={0}
+                                        position="absolute"
+                                        display="flex"
+                                        alignItems="center"
+                                        justifyContent="center"
+                                    >
+                                        <Typography component="div" style={{color: 'white', fontSize: '1rem'}}>{vote * 10}%</Typography>
+                                    </Box>
+                                </div>
+                                <Button color="secondary" startIcon={<PlayCircleFilledIcon />} variant="contained" component="a" href={`https://www.youtube.com/watch?v=${key}`} target="_blank" style={{padding: '9px 16px', marginLeft: 15}}>Play Trailer</Button>
+                            </div>
+                            <Typography variant="h5">Overview</Typography>
+                            <Typography variant="body1">{overview && overview}</Typography>
                         </div>
                     </Container>
                 </div>
